b219f9bec46b7396ba6063afcae48b7755a95bce
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-11-15 16:54:27 +0530

    Fixed linter errors

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-const navMenu = document.querySelector(".menu-bar");
-const headerItem = document.getElementsByClassName("header-item");
-const closeme = document.querySelector(".closeme");
-const menuItem = document.getElementsByClassName("menu-items");
-const hamburger = document.querySelector(".header-item4");
+const navMenu = document.querySelector('.menu-bar');
+const headerItem = document.getElementsByClassName('header-item');
+const closeme = document.querySelector('.closeme');
+// const menuItem = document.getElementsByClassName('menu-items');
+const hamburger = document.querySelector('.header-item4');
 
-hamburger.addEventListener("click", () => {
-  Array.from(headerItem).forEach(function (element) {
-    element.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  Array.from(headerItem).forEach((element) => {
+    element.classList.toggle('active');
   });
-  navMenu.classList.toggle("active");
-})
+  navMenu.classList.toggle('active');
+});
 
-closeme.addEventListener("click", () => {
-  Array.from(headerItem).forEach(function (element) {
-    element.classList.toggle("active");
+closeme.addEventListener('click', () => {
+  Array.from(headerItem).forEach((element) => {
+    element.classList.toggle('active');
   });
-  navMenu.classList.toggle("active");
-})
+  navMenu.classList.toggle('active');
+});
