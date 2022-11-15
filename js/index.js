336c7700635b1a58431fc889040442ed0c5c6340
jaredatandi <jaredatandi07@gmail.com>
2022-11-15 17:08:48 +0300

    Get the section class for the active section function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const navMenu = document.querySelector('.menu-bar');
 const headerItem = document.getElementsByClassName('header-item');
 const closeme = document.querySelector('.closeme');
-// const menuItem = document.getElementsByClassName('menu-items');
+const menuItem = document.getElementsByClassName('menu-items');
 const hamburger = document.querySelector('.header-item4');
 
 hamburger.addEventListener('click', () => {
